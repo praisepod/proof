423322578f4af8390a720cdb877cc1e85bb098de
atesh87 <emogeekfreak@outlook.com>
2013-02-08 11:41:31 +0000

    Code Comments

--- PraisePodPoc.py
+++ PraisePodPoc.py
@@ -1,13 +1,20 @@
 import cv2.cv as cv
-import datetime
 
-fps = 10.0
-font = cv.InitFont(cv.CV_FONT_HERSHEY_COMPLEX_SMALL, 1, 1, 0, 1, 1)
-fontTitle = cv.InitFont(cv.CV_FONT_HERSHEY_COMPLEX_SMALL, 0.8, 0.8, 0, 1, 1)
+# --Todo--
+# Record Interface Button
+# Debug info
 
-def StartCam(capture, frame):
+def StartCam():
 	#cv.NamedWindow("PraisePod (Proof Of Concept)", 1)
 	recording = False
+	
+	capture = cv.CaptureFromCAM(0)
+	frame = cv.RetrieveFrame(capture)
+	frame_size = cv.GetSize(frame)
+	fps = 10.0																			# FPS Set low to reduce output video speed.
+	
+	font = cv.InitFont(cv.CV_FONT_HERSHEY_COMPLEX_SMALL, 1, 1, 0, 1, 1)
+	fontTitle = cv.InitFont(cv.CV_FONT_HERSHEY_COMPLEX_SMALL, 0.8, 0.8, 0, 1, 1)
 	
 	writer = cv.CreateVideoWriter("out.avi", cv.CV_FOURCC('D', 'I', 'V', 'X'), 
 						  fps, frame_size, True)
@@ -16,10 +23,12 @@ def StartCam(capture, frame):
 		img = cv.QueryFrame(capture)
 		cv.PutText(frame,"PraisePod - Proof Of Concept", (frame_size[0] / 2,30), fontTitle, (0,0,255))
 		if recording == False:
-			cv.PutText(frame,"Press space to start recording.", (10,frame_size[1] - 10), font, (0,0,255))
+			cv.PutText(frame,"Press space to start recording.", (10,frame_size[1] - 10), font, (0,0,255))			# To be replaced with interface button.
 		else:
 			cv.PutText(frame,"Recording...", (10,frame_size[1] - 10), font, (0,0,255))
 		cv.ShowImage("camera", img)
+		
+		# Write Frames once recording is enabled.
 		if recording == True:
 			frame = cv.RetrieveFrame(capture)
 			cv.WriteFrame(writer, frame)
@@ -35,7 +44,4 @@ def StartCam(capture, frame):
 if __name__ == '__main__':
 	print("PraisePod (Proof Of Concept)")
 	raw_input("Press Enter to start...")
-	capture = cv.CaptureFromCAM(0)
-	frame = cv.RetrieveFrame(capture)
-	frame_size = cv.GetSize(frame)
-	StartCam(capture, frame)
+	StartCam()
