899992674e0b4667640f4a7802ecb238ac68b171
atesh87 <emogeekfreak@outlook.com>
2013-03-04 16:02:02 +0000

    Audio Disabled Until ALSO resolved

--- PraisePodPoc.py
+++ PraisePodPoc.py
@@ -1,5 +1,5 @@
 import cv2.cv as cv
-import pyaudio
+#import pyaudio
 import wave
 import os
 
@@ -14,13 +14,13 @@ class Camera:
 		self.defaultFont = cv.InitFont(cv.CV_FONT_HERSHEY_COMPLEX_SMALL, 1, 1, 0, 1, 1)
 		self.titleFont = cv.InitFont(cv.CV_FONT_HERSHEY_COMPLEX_SMALL, 0.8, 0.8, 0, 1, 1)
 		
-		self.CHUNK = 1024
-		self.FORMAT = pyaudio.paInt16
-		self.CHANNELS = 2
-		self.RATE = 44100
+		#self.CHUNK = 1024
+		#self.FORMAT = pyaudio.paInt16
+		#self.CHANNELS = 2
+		#self.RATE = 44100
 		self.output_filename_base = "temp"
 		
-		self.pyA = pyaudio.PyAudio()
+		#self.pyA = pyaudio.PyAudio()
 	
 	def StartCam(self):
 		if self.frame is not None:			
@@ -30,23 +30,26 @@ class Camera:
 				
 	def RecordAV(self):
 			frame_size = cv.GetSize(self.frame)
-			fps = 10.0																			# FPS Set low to reduce output video speed.
+			fps = 10.0
+			cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FPS, 25)
+			cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_WIDTH, 320)
+			cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 240)																		# FPS Set low to reduce output video speed.
 			
-			writer = cv.CreateVideoWriter("temp/" + self.output_filename_base + ".avi", cv.CV_FOURCC('D', 'I', 'V', 'X'), 
+			writer = cv.CreateVideoWriter("temp/" + self.output_filename_base + ".avi", cv.CV_FOURCC('F', 'L', 'V', '1'), 
 								  fps, frame_size, True)
 			
-			stream = self.pyA.open(format=self.FORMAT,
-					channels=self.CHANNELS,
-					rate=self.RATE,
-					input=True,
-					frames_per_buffer=self.CHUNK)
+			#stream = self.pyA.open(format=self.FORMAT,
+			#		channels=self.CHANNELS,
+			#		rate=self.RATE,
+			#		input=True,
+			#		frames_per_buffer=self.CHUNK)
 		
-			frames = []
+			#frames = []
 		
 			#Main Display/Record Loop
 			while True:
 				img = cv.QueryFrame(self.capture)
-				cv.PutText(self.frame,"PraisePod - Proof Of Concept", (frame_size[0] / 2,30), self.titleFont, (0,0,255))
+				cv.PutText(self.frame,"PraisePod - Proof Of Concept", (10,frame_size[1] - 10), self.defaultFont, (0,0,255))
 				if self.recording == False:
 					cv.PutText(self.frame,"Press space to start recording.", (10,frame_size[1] - 10), self.defaultFont, (0,0,255))			# To be replaced with interface button.
 				else:
@@ -57,24 +60,24 @@ class Camera:
 				if self.recording == True:
 					self.frame = cv.RetrieveFrame(self.capture)
 					cv.WriteFrame(writer, self.frame)
-					data = stream.read(self.CHUNK)
-					frames.append(data)
+					#data = stream.read(self.CHUNK)
+					#frames.append(data)
 				if cv.WaitKey(10) == 32:
 					if self.recording == False:
 						self.recording = True;
 					else:
 						self.recording = False;
 						
-						stream.stop_stream()
-						stream.close()
-						self.pyA.terminate()
+						#stream.stop_stream()
+						#stream.close()
+						#self.pyA.terminate()
 
-						wf = wave.open("temp/" + self.output_filename_base + ".wav", 'wb')
-						wf.setnchannels(self.CHANNELS)
-						wf.setsampwidth(self.pyA.get_sample_size(self.FORMAT))
-						wf.setframerate(self.RATE)
-						wf.writeframes(b''.join(frames))
-						wf.close()
+						#wf = wave.open("temp/" + self.output_filename_base + ".wav", 'wb')
+						#wf.setnchannels(self.CHANNELS)
+						#wf.setsampwidth(self.pyA.get_sample_size(self.FORMAT))
+						#wf.setframerate(self.RATE)
+						#wf.writeframes(b''.join(frames))
+						#wf.close()
 						
 						os.system("echo 'Combining Streams'")
 						os.system("ffmpeg -i temp/" + self.output_filename_base + ".wav -i temp/" + self.output_filename_base + ".avi -y -acodec copy -vcodec copy output.avi")
