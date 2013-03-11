de8cba292171d092bdd0f65bd22afa1b13d74c3d
Martyn Eggleton <martyn.eggleton@gmail.com>
2013-03-11 10:34:26 +0000

    Adds License

--- PraisePodPoc.py
+++ PraisePodPoc.py
@@ -1,3 +1,22 @@
+#  Copyright 2013 Richard Crook
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Project aims to develop a Raspberry Pi based working prototype of Praise Pod
+#
+#  @author Adam Tesh atesh87@outlook.com
+ 
+
 import cv2.cv as cv
 #import pyaudio
 import wave
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-proof
-=====
+Project aims to develop a Raspberry Pi based working prototype of Praise Pod
 
-proof of concept
+Copyright 2013 Richard Crook
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+
+@author Adam Tesh atesh87@outlook.com
